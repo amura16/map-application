f6e2fe3db5d93ec9ffc8c3b3d8ea3f9309fd6e92
amura16 <roddytanjaka16@gmail.com>
2026-08-13 11:38:26 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,11 @@ import routeRoutes from './routes/routeRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use('/api/locations', locationRoutes);
